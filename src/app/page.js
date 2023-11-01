8c47e7b6df86ca3530ff141d54787fc0ea83dc38
Lovepreet singh <lovepreetsingh1223@gmail.com>
2023-11-01 21:58:45 +0530

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,23 @@
+import ActionSection from "@/components/homepage/ActionSection";
+import ContactForm from "@/components/homepage/ContactForm";
+import FeatureSection from "@/components/homepage/FeatureSection";
+import BannerSection from "@/components/homepage/HomeBanner";
+import TestimonialSection from "@/components/homepage/TestimonialSection";
+
+export const metadata = {
+  title: "Home : Work Manager",
+};
+
+export default function Home() {
+  return (
+    <div>
+      {/* banner section  */}
+
+      <BannerSection />
+      <FeatureSection />
+      <ActionSection />
+      <TestimonialSection />
+      <ContactForm />
+    </div>
+  );
+}
